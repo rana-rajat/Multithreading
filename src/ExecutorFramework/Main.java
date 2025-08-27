02c750edf9a67c7e6a19c995fff23ea356af6209
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-08-27 22:54:37 +0530

    We WE need Threads

---
+++ src/ExecutorFramework/Main.java
@@ -0,0 +1,60 @@
+package ExecutorFramework;
+
+public class Main {
+    public static int factorial(int n) {
+        int count = 1;
+        for (int i = 1; i < n; i++) {
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                throw new RuntimeException(e);
+            }
+            count *= i;
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        //without multi
+        for (int i = 1; i < 10; i++) {
+            System.out.println(factorial(i));
+        }
+        // with MultiThreading
+        long startTime = System.currentTimeMillis();
+        Thread[] threads = new Thread[9];
+        for (int i = 1; i < 10; i++) {
+            int finalI = i; // need to declare the final details are below
+            threads[i - 1] = new Thread(() -> {
+                long result = factorial(finalI);
+                System.out.println(result);
+            });
+            threads[i - 1].start();
+        }
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+            }
+        }
+        // without threading taking 9 seconds now its taking only 1 second
+        System.out.println("Total Time " + (System.currentTimeMillis() - startTime));
+    }
+}
+//Here, i is a loop variable. What happens is:
+//
+//The lambda (() -> {}) doesn’t run immediately.
+//
+//It gets executed later when the thread starts.
+//
+//By that time, the loop may have already incremented i — possibly all the way to 10.
+//
+//So instead of each thread capturing its own i, they all may end up using the final value of i (10) when they finally run.
+
+//Each iteration creates a new variable finalI, which holds the current value of i.
+//
+//That value is fixed (not changed anymore).
+//
+//Each thread captures its own finalI.
+//
+//No matter when the thread executes, it always sees the correct value.
