317ec54311e983b5529b5b4e10489f919427400e
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-08-29 19:02:10 +0530

    Learning Executor Framework

--- src/ExecutorFramework/FirstExecutors.java
+++ src/ExecutorFramework/FirstExecutors.java
@@ -1,14 +1,13 @@
 package ExecutorFramework;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import java.util.concurrent.*;
 
 public class FirstExecutors {
     public static int factorial(int n) {
         int count = 1;
         for (int i = 1; i < n; i++) {
             try {
-                Thread.sleep(1000);
+                Thread.sleep(100);
             } catch (InterruptedException e) {
                 throw new RuntimeException(e);
             }
@@ -20,15 +19,23 @@ public class FirstExecutors {
     public static void main(String[] args) {
         // with MultiThreading
         long startTime = System.currentTimeMillis();
+        // Executor executor = Executors.newFixedThreadPool(3);
         ExecutorService executor = Executors.newFixedThreadPool(3);
         for (int i = 1; i < 10; i++) {
             int finalI = i;
-            executor.submit(() -> {
+            Future<?> future = executor.submit(() -> {
                 long result = factorial(finalI);
                 System.out.println(result);
             });
         }
-        executor.shutdown();
+        executor.shutdown(); //to stop thread pool and after this no thread can be started will give exception
+//        try {
+//           blocks until all the tasks have executed after a shutdown request to
+//           below we will wait for 100 seconds to complete executor shutdown if it is not move to next statement
+//            executor.awaitTermination(100, TimeUnit.MICROSECONDS);
+//        } catch (InterruptedException e) {
+//            throw new RuntimeException(e);
+//        }
         System.out.println("Total Time " + (System.currentTimeMillis() - startTime));
     }
 }
