bbf2cfae5f1faf5078c5136ff93d1340d94d5292
rana-rajat <rajatsatbirsingh28052000r@gmail.com>
2025-08-31 22:48:50 +0530

    Update FifthInvokeMethod.java

--- src/ExecutorFramework/FifthInvokeMethod.java
+++ src/ExecutorFramework/FifthInvokeMethod.java
@@ -1,6 +1,5 @@
 package ExecutorFramework;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.concurrent.Callable;
@@ -9,23 +8,36 @@ import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class FifthInvokeMethod {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) throws Exception {
+        // Create a thread pool of size 2
         ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        // Define callable tasks
         Callable<Integer> callable1 = () -> {
-            System.out.println("Task1");
+            System.out.println("Task1 executed by " + Thread.currentThread().getName());
             return 1;
         };
         Callable<Integer> callable2 = () -> {
-            System.out.println("Task2");
+            System.out.println("Task2 executed by " + Thread.currentThread().getName());
             return 2;
         };
         Callable<Integer> callable3 = () -> {
-            System.out.println("Task3");
+            System.out.println("Task3 executed by " + Thread.currentThread().getName());
             return 3;
         };
 
+        // Put tasks into a list
         List<Callable<Integer>> list = Arrays.asList(callable1, callable2, callable3);
+
+        // Submit all tasks together using invokeAll()
         List<Future<Integer>> futures = executorService.invokeAll(list);
+
+        // Retrieve results
+        for (Future<Integer> future : futures) {
+            System.out.println("Result: " + future.get());
+        }
+
+        // Shutdown the executor
         executorService.shutdown();
     }
 }
